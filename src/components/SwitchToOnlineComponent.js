ee767dc64d373b4156fa3bb1f7d935f2f1fd9c58
coolpinkzz <pratik.1rn17ee029@gmail.com>
2020-08-12 01:12:40 +0530

    sto bg edited

--- src/components/SwitchToOnlineComponent.js
+++ src/components/SwitchToOnlineComponent.js
@@ -6,77 +6,111 @@ const Switchtoonline = () => {
   return (
     <div>
       {" "}
-      <section className="main">
-        <Fade bottom duration={1000} distance="40px">
-          <div className="container-fluid">
-            <div className="row">
-              <div className="moto col-sm-12 col-md-6 col-lg-6">
-                <div className="pt-5">
-                  <h1 style={{ color: "#55198b" }}>Switch to Online</h1>
-                  <hr color="#ff458c"></hr>
-                  <h5>
-                    Looking to take your offline business to online world? Let
-                    us help you get there. Let us worry about all the work to
-                    take your business online. We will make your personal
-                    website with the exact information, requirements and
-                    products. By applying safe methods of online financial work
-                    through us, you can track your profit without any hurdle.
-                  </h5>
-                </div>
-              </div>
-
-              <div className="col-12 col-sm-12 col-md-6 col-lg-6">
-                <div className="card-img">
-                  <img
-                    className="main-image img-fluid"
-                    src="images/sto.png"
-                    alt="Switch-to-online"
-                  />
-                </div>
+      <div className="container-fluid">
+        <div className="row ">
+          <Fade left duration={1000} distance="40px">
+            <div className="moto col-sm-12 col-md-6 col-lg-6">
+              <div className="pt-5">
+                <h1 style={{ color: "#55198b" }}>Switch to Online</h1>
+                <hr color="#ff458c"></hr>
+                <h5>
+                  Looking to take your offline business to online world? Let us
+                  help you get there. Let us worry about all the work to take
+                  your business online. We will make your personal website with
+                  the exact information, requirements and products. By applying
+                  safe methods of online financial work through us, you can
+                  track your profit without any hurdle.
+                </h5>
               </div>
             </div>
-          </div>
-        </Fade>
-      </section>
-      <section className="poten">
-        <div className="container">
-          <div className="row text">
-            <h1 style={{ fontSize: "20px" }}>
-              The many benefits of online business include:
-            </h1>
-
-            <div className="col-12 col-md-6 col-sm-6">
-              <ul>
-                <li>global access, 24 hours a day, 7 days a week</li>
-                <li>improved client service through greater flexibility</li>
-                <li>cost savings</li>
-                <li> faster delivery of products</li>
-                <li>less paper waste</li>
-                <li>increased professionalism</li>
-                <li>
-                  opportunities to manage your business from anywhere in the
-                  world
-                </li>
-              </ul>
+          </Fade>
+          <Fade right duration={1000} distance="40px">
+            <div className="col-12 col-sm-12 col-md-6 col-lg-6">
+              <div className="card-img">
+                <img
+                  className="main-image img-fluid"
+                  src="images/sto.png"
+                  alt="Switch-to-online"
+                />
+              </div>
             </div>
-            <div className="col-12 col-md-6 col-sm-6">
-              <ul>
-                <li>run an online shop</li>
-                <li>manage your suppliers</li>
-                <li>communicate with your customers, and get their feedback</li>
-                <li>offer services online</li>
-                <li>
-                  allow customers to make reservations or appointments online
-                </li>
-                <li>
-                  manage your finances, such as online banking, tax and employee
-                </li>
-                <li>research competitors.</li>
-              </ul>
+          </Fade>
+        </div>
+      </div>
+      <div className="sto-2">
+        <div className="overlay">
+          <div className=" container ">
+            <div className="sto-2-inside row text p-5">
+              <h1 style={{ fontSize: "30px" }}>
+                The many benefits of online business include:
+              </h1>
+
+              <div className="col-12 col-md-6 col-sm-6">
+                <ul>
+                  <li>
+                    <p>global access, 24 hours a day, 7 days a week</p>
+                  </li>
+                  <li>
+                    <p>improved client service through greater flexibility</p>
+                  </li>
+                  <li>
+                    <p>
+                      improved client service through greater flexibilitycost
+                      savings
+                    </p>
+                  </li>
+                  <li>
+                    <p>faster delivery of products</p>
+                  </li>
+                  <li>
+                    <p>less paper waste</p>
+                  </li>
+                  <li>
+                    <p>increased professionalism</p>
+                  </li>
+                  <li>
+                    <p>
+                      opportunities to manage your business from anywhere in the
+                      world
+                    </p>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-12 col-md-6 col-sm-6">
+                <ul>
+                  <li>
+                    <p>manage your suppliers</p>
+                  </li>
+                  <li>
+                    <p>
+                      communicate with your customers, and get their feedback
+                    </p>
+                  </li>
+                  <li>
+                    <p>offer services online</p>
+                  </li>
+                  <li>
+                    <p>
+                      allow customers to make reservations or appointments
+                      online
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      {" "}
+                      manage your finances, such as online banking, tax and
+                      employee
+                    </p>
+                  </li>
+                  <li>
+                    <p>research competitors.</p>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
     </div>
   );
 };
